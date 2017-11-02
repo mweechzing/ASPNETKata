974d71eac971693b9c80125ee134f25c56a55755
Micah Weech <mweechzing@gmail.com>
2017-11-02 12:50:17 -0500

    added person model

---
+++ ASPNETKata/Models/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASPNETKata.Models
+{
+    public class Person
+    {
+        public int Age { get; set; }
+        public bool IsMinor { get; set; }
+    }
+}
